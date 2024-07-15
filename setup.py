60257b278cfbffe3cfe4d7466754510022709f17
Miles Barr <1018475+milesbarr@users.noreply.github.com>
2024-07-15 07:15:32 -0700

    Fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="A Python package to translate website assets.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/wearesystematic/website-translator",
+    url="https://github.com/wearesystematic/translator",
     packages=find_packages(),
     install_requires=["beautifulsoup4", "deep_translator"],
     entry_points={
